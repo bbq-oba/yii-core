55cd29ef72363afeeffdd8cf217748c8cba21978
oyoy8629@qq.com <oyoy8629@qq.com>
2016-02-13 02:15:48 +0800

    sms

---
+++ config/smsdb.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=c144g86558.iask.in;dbname=sms',
+    'username' => 'sms',
+    'password' => 'smssmssms123',
+    'charset' => 'utf8',
+///    'tablePrefix' => TABLE_PREFIX,
+];
