b2591d2772ce1e79daa7711277c0f58c9cc210b6
oyoy8629@qq.com <oyoy8629@qq.com>
2016-02-19 02:26:14 +0800

    .

--- config/smsdb.php
+++ config/smsdb.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=1449y969z3.51mypc.cn;dbname=sms',
-    'username' => 'root',
-    'password' => 'Wannagirl.',
+    'dsn' => 'mysql:host=1449y969z3.51mypc.cn;port=41372;dbname=sms',
+    'username' => 'sms',
+    'password' => 'smssmssms123',
     'charset' => 'utf8',
 ///    'tablePrefix' => TABLE_PREFIX,
 ];
