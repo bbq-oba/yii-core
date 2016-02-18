c7bed93da916d46eb5349d2884e44dd639f36520
oyoy8629@qq.com <oyoy8629@qq.com>
2016-02-19 00:51:17 +0800

    no message

--- config/smsdb.php
+++ config/smsdb.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=1449y969z3.51mypc.cn;port=11953;dbname=sms',
-    'username' => 'sms',
-    'password' => 'smssmssms123',
+    'dsn' => 'mysql:host=1449y969z3.51mypc.cn;dbname=sms',
+    'username' => 'root',
+    'password' => 'Wannagirl.',
     'charset' => 'utf8',
 ///    'tablePrefix' => TABLE_PREFIX,
 ];
