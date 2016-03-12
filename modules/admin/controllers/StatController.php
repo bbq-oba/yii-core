d0512a255f08c1be99738553355b8931cc05072f
oyoy8629@qq.com <oyoy8629@qq.com>
2016-03-12 16:55:08 +0800

    a

--- modules/admin/controllers/StatController.php
+++ modules/admin/controllers/StatController.php
@@ -4,7 +4,9 @@ namespace app\modules\admin\controllers;
 
 use app\api\core\API;
 use app\helpers\Page;
+use app\modules\admin\models\ApiVisitorDetail;
 use yii\data\ArrayDataProvider;
+use yii\helpers\ArrayHelper;
 use yii\web\Response;
 
 class StatController extends \yii\web\Controller
@@ -29,7 +31,6 @@ class StatController extends \yii\web\Controller
         ]);
     }
     public function actionRegUser(){
-
         $params['filter_offset'] = max(0,\yii::$app->request->get('filter_offset',0));
         $params['filter_limit'] = 20;
         $params['segment'] = 'userId!=';
@@ -37,12 +38,87 @@ class StatController extends \yii\web\Controller
             $params['segment'] = 'visitorId=='.$visitorId;
         }
         $data = API::run('Live.getLastVisitsDetails',$params);
+        $data = $this->getDb($data);
 
         $dataProvider = new ArrayDataProvider(['allModels' => $data]);
         return $this->render('reg-user',[
             'dataProvider' => $dataProvider
         ]);
     }
+
+    public function actionUpdateRegUser(){
+        $params['filter_offset'] = max(0,\yii::$app->request->get('filter_offset',0));
+        $params['filter_limit'] = 20;
+        $params['segment'] = 'userId!=';
+        if(!empty($visitorId)){
+            $params['segment'] = 'visitorId=='.$visitorId;
+        }
+        $data = API::run('Live.getLastVisitsDetails',$params);
+        $this->format($data);
+
+    }
+
+
+
+    public function getDb($data){
+        $userArray = [];
+
+        //找到piwik 返回数据的 userId
+        foreach($data as $row){
+            $userArray[] = $row["userId"];
+        }
+        $userArray = array_unique($userArray);
+
+        // 在数据库中查找 username
+        $find = ApiVisitorDetail::find()->where([
+            "in","visitor_username",$userArray
+        ])->asArray()->all();
+        $array = [];
+
+        //格式化username
+        foreach($find as $k=>$v){
+            $array[$v["visitor_referrer"]."---".$v["visitor_username"]] = $v;
+        }
+
+        //匹配到data中返回
+        foreach($data as $k=>$row){
+            if(isset($row["customVariables"]["customVariableName2"])
+                && $row["customVariables"]["customVariableName2"] == "regReferrer"
+                && isset($row["customVariables"]["customVariableValue2"])
+                && !empty($row["customVariables"]["customVariableValue2"])
+                && in_array($row["customVariables"]["customVariableValue2"],array_keys(ApiVisitorDetail::$referrerType))
+                && in_array($row["customVariables"]["customVariableValue2"]."---".$row["userId"],array_keys($array))
+            ){
+                $data[$k] = array_merge($row,$array[$row["customVariables"]["customVariableValue2"]."---".$row["userId"]]);
+            }
+        }
+
+        return $data;
+    }
+
+    public function format($data){
+        $array = [];
+        foreach($data as $row){
+            if(isset($row["customVariables"]["customVariableName2"])
+                && $row["customVariables"]["customVariableName2"] == "regReferrer"
+                && isset($row["customVariables"]["customVariableValue2"])
+                && !empty($row["customVariables"]["customVariableValue2"])
+                && in_array($row["customVariables"]["customVariableValue2"],array_keys(ApiVisitorDetail::$referrerType))
+            ){
+                $array[$row["customVariables"]["customVariableValue2"]][] = $row["userId"];
+            }
+        }
+
+        if($array){
+            $api = new ApiVisitorDetail();
+            foreach($array as $referrer => $userArray){
+                $api->getUserAllData(array_unique($userArray),$referrer);
+            }
+        }
+    }
+
+
+
     public function actionVisitorProfile($visitorId){
 
         $params['segment'] = 'visitorId=='.$visitorId;
