0e9f58004a8e539261a0c4f17a56cca05f2e0fe6
oyoy8629@qq.com <oyoy8629@qq.com>
2016-03-13 00:33:48 +0800

    ..

--- config/define.php
+++ config/define.php
@@ -7,7 +7,7 @@
  */
 
 define("SERVER_ENV_PREFIX",'WO2365_');
-define('TABLE_PREFIX','pre_');
+define('TABLE_PREFIX','vip_');
 /**
  * CURRENT_TIMESTAMP 定义为当前时间，减少框架调用 time() 的次数
  */
@@ -15,11 +15,9 @@ define('CURRENT_TIMESTAMP', $_SERVER['REQUEST_TIME']);
 define('CURRENT_DATETIME', date('Y-m-d H:i:s',CURRENT_TIMESTAMP));
 define('CURRENT_DATE', date('Y-m-d',CURRENT_TIMESTAMP));
 
+define('STAT_TRACKER_URL','//p.sasa8.com/piwik.php');
 
-define('STAT_TRACKER_URL','//'.$_SERVER['HTTP_HOST']."/piwik.php");
-
-
-define('STAT_API_URL','http://'.$_SERVER['HTTP_HOST'].'/api.php');
+define('STAT_API_URL','//p.sasa8.com/piwik.php');
 define('STAT_API_TOKEN','c38de7c5e14711949af48b11464d8cba');
 
 define('DEFAULT_ID_SITE',1);
