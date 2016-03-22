d14474e08c96ebc9493e2f9b03f646e413002811
oyoy8629 <oyoy8629@qq.com>
2016-03-23 01:45:52 +0800

    a

--- modules/admin/views/stat/reg-user.php
+++ modules/admin/views/stat/reg-user.php
@@ -12,17 +12,6 @@ use \app\helpers\Column;
 
 $this->title = $title;
 $this->params['breadcrumbs'][] = $this->title;
-<<<<<<< HEAD
-//Yii::$app->timeZone = 'UTC';
-$type = \yii::$app->request->get("type");
-
-$panel = '<div class="btn-group" role="group">'.
-    Html::a("更新用户API数据", ['update-reg-user','filter_limit'=>\yii::$app->request->get('filter_limit'),'filter_offset'=>\yii::$app->request->get('filter_offset')]).
-'</div>';
-//&filter_offset=40&filter_limit=20
-=======
->>>>>>> 558994698949798980a1cd203dd7f3ff66170cd2
-
 ?>
 
 
