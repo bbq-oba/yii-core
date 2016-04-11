d680b42cf4d52640cd7a78f378a7c9939327bf0f
oyoy8629 <oyoy8629@qq.com>
2016-04-11 22:08:53 +0800

    ref

--- modules/admin/models/VisitsDetails.php
+++ modules/admin/models/VisitsDetails.php
@@ -154,6 +154,8 @@ class VisitsDetails extends Model
             $data[$k]['ip'] = $array ? $array['ip'] : '';
             $data[$k]['iptype'] = $array ? $array['iptype'] : '';
             $data[$k]['iptext'] = $array ? $array['iptext'] : '';
+            
+            $data[$k]['visitor_referrer'] = $array ? $array['visitor_referrer'] : '';
 
             for ($i = 0 ; $i < 10 ; $i++) {
                 $data[$k]['visitor_datatype_' . $i] = ($array ? $array['visitor_datatype_' . $i] : '');
