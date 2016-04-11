e3d71350261cccab0c48d27a3c168edfc30ff641
oyoy8629 <oyoy8629@qq.com>
2016-04-11 21:48:03 +0800

    2222

--- helpers/UserLogic.php
+++ helpers/UserLogic.php
@@ -12,10 +12,12 @@ namespace app\helpers;
 use app\models\ApiVisitorConfig;
 use app\models\ApiVisitorDetail;
 use app\models\StatLogVisit;
+use Curl\Curl;
+use yii\base\Object;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Console;
 
-class UserLogic extends UserService
+class UserLogic extends Object
 {
 
     public $config = [];
@@ -71,6 +73,7 @@ class UserLogic extends UserService
                 ];
 
             }
+
 //            \yii::error(var_export($array,1));
             //查找所有已经存在的记录
             $batchInsert = [];
@@ -131,7 +134,7 @@ class UserLogic extends UserService
                 'fromTime' => $fromTime,
                 'toTime'   => $toTime,
             ];
-            $return = UserService::get($ref,$type,$params);
+            $return = $this->get($ref,$type,$params);
 //            $return = [
 //                'code' =>200 ,
 //                'data' =>'a'
@@ -143,7 +146,7 @@ class UserLogic extends UserService
                 if(!$model->update()){
                     print_r($model->errors);
                 }else{
-                    \yii::$app->controller->stdout(sprintf("%s - %s:%s Id:%d \n",$userName , UserService::$typeEnum[$type][0] , $return['data'],$model->id), Console::BOLD);
+                    \yii::$app->controller->stdout(sprintf("%s - %s:%s Id:%d \n",$userName , $this->config['name'] , $return['data'],$model->id), Console::BOLD);
                 }
             }else{
                 $i++;
@@ -153,5 +156,106 @@ class UserLogic extends UserService
             }
         }
     }
+    public static $refEnum = [
+        1=>[
+            'url' => 'lbvbet',
+            'txt' => '乐宝'
+        ],
+        2=>[
+            'url' => 'wyvbet',
+            'txt' => '永利汇'
+        ]
+    ];
 
+    const SECRET_KEY = '604A0B84-FBAD-4B45-AF2D-E1F848CD543F';
+
+
+    public static $typeEnum  = [
+        0 => ['所属推广号','api/Extension/ReferralCode'],
+        1 => ['用户首存金额','api/Extension/FirstDepositAmount'],
+        2 => ['用户首存优惠','api/Extension/FirstDepositBonus'],
+        3 => ['用户存款笔数','api/Extension/DepositCount'],
+        4 => ['登录时间','api/Extension/LastLogin'],
+        5 => ['成功提款次数','api/Extension/WithdrawalCount'],
+        6 => ['会员投注信息',' api/Extension/BetAmount'],
+        7 => ['未存款之前领取的优惠'],
+        8 => ['所有优惠'],
+    ];
+
+
+//    /**
+//     * @param $ref int
+//     * @param $type int
+//     * @return string
+//     */
+    public  function makeUrl($ref,$type){
+        return 'http://'.self::$refEnum[$ref]['url'].'.gallary.work/'.$this->config['url'];
+    }
+
+
+
+    //生成签名
+    public static function makeSign($params){
+        $params['timestamp'] = date('Y-m-d H:i:s',CURRENT_TIMESTAMP);
+        $params['secretKey'] = self::SECRET_KEY;
+        $params['sign'] = md5(self::buildQuery($params));
+
+        unset($params['secretKey']);
+        return $params;
+    }
+
+
+    public function get($ref , $type , $params){
+        $params = array_filter($params,function($val){
+            return $val !== null;
+        });
+        $url = $this->makeUrl($ref,$type);
+        $params = self::makeSign($params);
+        return self::run($url,$params);
+    }
+
+    public static function buildQuery($params){
+        $paramsJoined = [];
+        foreach($params as $param => $value) {
+            $paramsJoined[] = "$param=$value";
+        }
+        $query = implode('&', $paramsJoined);
+        return $query;
+    }
+    public static function run($url , $params){
+        $curl = new Curl();
+        $curl->setJsonDecoder(function($response) {
+            $json_obj = json_decode($response, true);
+            if (!($json_obj === null)) {
+                $response = $json_obj;
+            }
+            return $response;
+        });
+        $curl->get($url,$params);
+        $curl->setConnectTimeout(10);
+        $curl->close();
+        return self::handleResponse($curl);
+    }
+    public static function handleResponse($curl){
+        $return = [
+            'code' => 0,
+            'msg'  => '',
+            'data' => null,
+        ];
+        if ($curl->error) {
+            return $return;
+        } else {
+            if($curl->response === false){
+                return $return;
+            }
+            if($curl->response['StatusCode'] == 0){
+                $return['code'] = 200;
+            }else{
+                $return['code'] = 0;
+            }
+            $return['msg']  = $curl->response['Message'];
+            $return['data'] = $curl->response['Data'];
+            return $return;
+        }
+    }
 }
--- modules/admin/helpers/StatColumns.php
+++ modules/admin/helpers/StatColumns.php
@@ -11,7 +11,9 @@ namespace app\modules\admin\helpers;
 use app\helpers\IP;
 use app\helpers\IpType;
 use app\helpers\RegUser;
+use app\helpers\UserLogic;
 use app\helpers\UserService;
+use app\models\ApiVisitorConfig;
 use app\modules\admin\models\ApiVisitorDetail;
 use kartik\grid\GridView;
 use \kartik\helpers\Html;
@@ -19,21 +21,29 @@ use \kartik\helpers\Html;
 class StatColumns
 {
     static $columns = [];
-    public static function Columns1(){
-        self::$columns[] =['class' => 'yii\grid\SerialColumn'];
+
+    public static function Columns1()
+    {
+        self::$columns[] = ['class' => 'yii\grid\SerialColumn'];
     }
-    public static function Columns2(){
-        self::$columns[] =[ 'header' => '访问时间',
+
+    public static function Columns2()
+    {
+        self::$columns[] = ['header' => '访问时间',
             'value' => function ($data) {
-                return date("Y-m-d H:i:s",$data["serverTimestamp"]);
+                return date("Y-m-d H:i:s", $data["serverTimestamp"]);
             },
         ];
     }
-    public static function Columns3(){
-        self::$columns[] =['header' => '唯一标识', 'value' => 'visitorId'];
+
+    public static function Columns3()
+    {
+        self::$columns[] = ['header' => '唯一标识', 'value' => 'visitorId'];
     }
-    public static function Columns4(){
-        self::$columns[] =['header' => '访客类别',
+
+    public static function Columns4()
+    {
+        self::$columns[] = ['header' => '访客类别',
             'value' => function ($data) {
                 if ($data['visitorType'] == 'new') {
                     return '新访客';
@@ -41,36 +51,46 @@ class StatColumns
                     return '老访客<br />第' . $data['visitCount'] . '次访问';
                 }
             },
-            'format'=>'raw',
-            'headerOptions'=>[
-                'style'=>'width:100px;'
+            'format' => 'raw',
+            'headerOptions' => [
+                'style' => 'width:100px;'
             ]
         ];
     }
-    public static function Columns5(){
-        self::$columns[] = ['header' => '访客Ip','value' => 'visitIp'];
+
+    public static function Columns5()
+    {
+        self::$columns[] = ['header' => '访客Ip', 'value' => 'visitIp'];
     }
-    public static function Columns6(){
-        self::$columns[] =[ 'header'=>'ip归属地',
-            'value'=>function($data){
-                return implode(" ",IP::find($data["visitIp"]));
+
+    public static function Columns6()
+    {
+        self::$columns[] = ['header' => 'ip归属地',
+            'value' => function ($data) {
+                return implode(" ", IP::find($data["visitIp"]));
             },
         ];
     }
-    public static function Columns7(){
-        self::$columns[] =['header' => '关键词','value' => 'referrerKeyword'];
+
+    public static function Columns7()
+    {
+        self::$columns[] = ['header' => '关键词', 'value' => 'referrerKeyword'];
 
     }
-    public static function Columns8(){
-        self::$columns[] =[
+
+    public static function Columns8()
+    {
+        self::$columns[] = [
             'header' => '来源url',
             'format' => 'raw',
-            'value' => function($data){
-                return '<div style="width: 300px; overflow: hidden;">'.$data['referrerUrl'].'</div>';
+            'value' => function ($data) {
+                return '<div style="width: 300px; overflow: hidden;">' . $data['referrerUrl'] . '</div>';
             },
         ];
     }
-    public static function Columns9(){
+
+    public static function Columns9()
+    {
 //        self::$columns[] =['header' => '关键词',
 //            'format' => 'raw',
 //            'value' => function ($data) {
@@ -79,33 +99,34 @@ class StatColumns
 //        ];
     }
 
-    public static function Columns10(){
-        self::$columns[] =['header' => '落地页Url',
+    public static function Columns10()
+    {
+        self::$columns[] = ['header' => '落地页Url',
             'format' => 'raw',
-            'value' => function($data){
-                if($data['actionDetails'] && count($data['actionDetails'])){
+            'value' => function ($data) {
+                if ($data['actionDetails'] && count($data['actionDetails'])) {
                     $pointUrl = array_shift($data['actionDetails']);
-                    return '<div style="width: 300px; overflow: hidden;">'.$pointUrl["url"].'</div>';
+                    return '<div style="width: 300px; overflow: hidden;">' . $pointUrl["url"] . '</div>';
                 }
                 return "";
             },
-            'options'=>[
-                'style'=>'width:300px; overflow:hidden;'
+            'options' => [
+                'style' => 'width:300px; overflow:hidden;'
             ]
         ];
     }
-
 
 
     public static function ColumnsIpType()
     {
         self::$columns[] = [
             'header' => 'Ip类型',
-            'value' => function($data){
+            'value' => function ($data) {
                 return $data['iptype'];
             }
         ];
     }
+
     public static function getCommonUserColumns()
     {
         self::Columns1();
@@ -125,12 +146,13 @@ class StatColumns
 
     public static function Columns11()
     {
-        self::$columns[] =['header' => '',
-            'value' => function($data){
+        self::$columns[] = ['header' => '',
+            'value' => function ($data) {
                 return '';
             }
         ];
     }
+
     public static function getRegUserColumns()
     {
         self::Columns1();
@@ -138,8 +160,8 @@ class StatColumns
         self::Columns3();
         self::Columns4();
         self::Columns5();
-        self::$columns[] =[ 'header'=>'ip归属地',
-            'value'=>function($data){
+        self::$columns[] = ['header' => 'ip归属地',
+            'value' => function ($data) {
                 return $data['iptext'];
             },
         ];
@@ -152,23 +174,8 @@ class StatColumns
 
         self::$columns [] = [
             'header' => '账号',
-            'value'  => 'userId',
+            'value' => 'userId',
             'filter' => GridView::TEXT
-        ];
-        self::$columns [] = [
-            'value' => function ($data) {
-                if (
-                    isset($data['customVariables']) && isset($data['customVariables'][1])
-                    && isset($data['customVariables'][1]['customVariableName1'])
-                    && $data['customVariables'][1]['customVariableName1'] == "regTime"
-                    && $data['customVariables'][1]['customVariableValue1'] > 0
-                ) {
-                    return date("Y-m-d H:i:s", $data['customVariables'][1]['customVariableValue1']);
-                } else {
-                    return "";
-                }
-            },
-            'header' => '注册时间'
         ];
         //self::$columns [] = [
         //    'value' => function ($data) {
@@ -178,17 +185,37 @@ class StatColumns
         //];
 
         // 0
+
         self::$columns [] = [
             'value' => function ($data) {
-                return  $data['visitor_datatype_0'];
+                $regTime = "";
+                if (
+                    isset($data['customVariables']) && isset($data['customVariables'][1])
+                    && isset($data['customVariables'][1]['customVariableName1'])
+                    && $data['customVariables'][1]['customVariableName1'] == "regTime"
+                    && $data['customVariables'][1]['customVariableValue1'] > 0
+                ) {
+                    $regTime = date("Y-m-d H:i:s", $data['customVariables'][1]['customVariableValue1']);
+                }
+                $array = [];
+
+                $regText = isset($data['visitor_referrer']) ? UserLogic::$refEnum[$data['visitor_referrer']] : "-";
+                $array[] = sprintf("注册时间:%s", $regTime);
+                $array[] = sprintf("注册来源:%s", $regText);
+                $array[] = sprintf("注册网址:%s", $data['visitor_datatype_9']);
+                $array[] = sprintf("代理号:%s", $data['visitor_datatype_0']);
+                $array[] = sprintf("首存金额:%s", $data['visitor_datatype_1']);
+                $array[] = sprintf("存款次数:%s", $data['visitor_datatype_3']);
+                return implode("<br />", $array);
             },
-            'header' => UserService::$typeEnum[0][0]
+            'header' => '推广信息',
+            'format' => 'raw'
         ];
         self::$columns [] = [
             'value' => function ($data) {
-                return  $data['visitor_datatype_1'];
+                return $data['visitor_datatype_1'];
             },
-            'header' => UserService::$typeEnum[1][0]
+            'header' => ApiVisitorConfig::cache()
         ];
 //        self::$columns [] = [
 //            'value' => function ($data) {
@@ -198,10 +225,33 @@ class StatColumns
 //        ];
         self::$columns [] = [
             'value' => function ($data) {
-                return  $data['visitor_datatype_3'];
+                return $data['visitor_datatype_2'];
             },
-            'header' => UserService::$typeEnum[3][0]
+            'header' => '首存优惠'
         ];
+        self::$columns [] = [
+            'value' => function ($data) {
+                return $data['visitor_datatype_4'];
+            },
+            'header' => '登录时间'
+        ];
+        self::$columns [] = [
+            'value' => function ($data) {
+                return $data['visitor_datatype_5'];
+            },
+            'header' => '提款次数'
+        ];
+        self::$columns [] = [
+            'value' => function ($data) {
+                $array = [];
+                $array[] = sprintf("次数:%s", $data['visitor_datatype_7']);
+                $array[] = sprintf("金额:%s", $data['visitor_datatype_8']);
+                return implode("<br />", $array);
+            },
+            'header' => '未存款优惠',
+            'format' => 'raw'
+        ];
+
 //        self::$columns [] = [
 //            'value' => function ($data) {
 //                return  $data['visitor_datatype_4'];
