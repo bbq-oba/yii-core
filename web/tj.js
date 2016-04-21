ef3423e58304a1a5f245db41bfeeaed9deff8272
oyoy8629 <oyoy8629@qq.com>
2016-04-21 22:12:38 +0800

    zzz

--- web/tj.js
+++ web/tj.js
@@ -3,9 +3,9 @@
  */
 var _paq = _paq || [];
 if(document.getElementById("RegisterSuccess") != null && document.getElementById("RegisterReferrer") != null){
+    _paq.push(['setUserId',document.getElementById("RegisterSuccess").value]);
     _paq.push(['setCustomVariable',1,'regTime',Date.parse(new Date())/1000,'visit']);
     _paq.push(['setCustomVariable',2,'regReferrer',document.getElementById("RegisterReferrer").value,'visit']);
-    _paq.push(['setCustomVariable',3,'regUser',document.getElementById("RegisterSuccess").value,'visit']);
 }
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
