ed02f0883cd91a6743686e18170299e0c7ac9c7a
oyoy8629 <oyoy8629@qq.com>
2016-04-23 11:05:00 +0800

    zz

--- helpers/SignLogic.php
+++ helpers/SignLogic.php
@@ -30,7 +30,7 @@ class SignLogic extends BaseLogic
 
     public function post($url,$params){
         $sign = self::makeSign($params);
-        $url = $url.'?timestamp='.CURRENT_TIMESTAMP.'&sign='.$sign;
+        $url = $url.'?timestamp='.date('Y-m-d H:i:s',CURRENT_TIMESTAMP).'&sign='.$sign;
         echo $url."\r\n";
         print_r($params);
         return self::run($url,$params,self::METHOD_POST);
