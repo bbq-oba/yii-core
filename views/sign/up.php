e4d1da9bbac67b5205ffea6b1f062062981ee3a2
oyoy8629 <oyoy8629@qq.com>
2016-06-01 23:08:11 +0800

    dsa

--- views/sign/up.php
+++ views/sign/up.php
@@ -41,16 +41,12 @@ $this->params['breadcrumbs'][] = $this->title;
                 ]
             ); ?>
             <div class="api-user-form box-body">
+                <?= $form->field($model, 'UserName')->textInput(['maxlength' => true,'autocomplete'=>'off']) ?>
+                <?= $form->field($model, 'Password')->passwordInput(['maxlength' => true]) ?>
                 <?= $form->field($model, 'Phone')->textInput(['maxlength' => true]) ?>
-
                 <?= $form->field($model, 'smsCode')->widget(\app\core\widgets\VerifySms::className(), [
                     'template' => '<div class="row"><div class="col-lg-6">{input}</div><div class="col-lg-6">{button}</div></div>',
                 ]) ?>
-
-                <?= $form->field($model, 'UserName')->textInput(['maxlength' => true,'autocomplete'=>'off']) ?>
-
-                <?= $form->field($model, 'Password')->passwordInput(['maxlength' => true]) ?>
-
             </div>
             <div class="box-footer" style="margin-top: 20px;">
                 <?= Html::submitButton('立即注册', ['class' => 'regBtn']) ?>
