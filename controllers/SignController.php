87b45432b7b97ebf69d6fbf450f8dd4b13c3673f
oyoy8629 <oyoy8629@qq.com>
2016-06-04 12:18:35 +0800

    c

--- controllers/SignController.php
+++ controllers/SignController.php
@@ -5,7 +5,9 @@ use app\helpers\Mobile;
 use app\helpers\SignLogic;
 use app\helpers\SmsHelper;
 use app\models\ApiUser;
+use kartik\form\ActiveForm;
 use Yii;
+use yii\helpers\Html;
 use yii\web\Controller;
 use yii\web\Response;
 
@@ -14,10 +16,50 @@ class SignController extends Controller
 
     public $layout = '@app/views/sign/layouts/sign';
 
+    public function actionCheck(){
+        $name = \yii::$app->request->post('name');
+        $value = \yii::$app->request->post('param');
+
+        \yii::$app->response->format = Response::FORMAT_JSON;
+
+        if($name == 'ApiUser[UserName]'){
+            $model = new ApiUser();
+
+            return $model->check('UserName',$value);
+        }
+        if($name == 'ApiUser[Phone]'){
+            $model = new ApiUser();
+            if(!Mobile::check($value)){
+                return ["info"=>"手机号格式有误","status"=>"n"];
+            }
+            return $model->check('Phone',$value);
+        }
+    }
+
+    public function actionCheckSmsCode(){
+
+        $mobile = \yii::$app->request->post('mobile');
+        $code = \yii::$app->request->post('code');
+
+        \yii::$app->response->format = Response::FORMAT_JSON;
+
+        $model = new ApiUser();
+
+        $model->Phone = $mobile;
+        $model->smsCode = $code;
+        return ["code"=>$model->validateSmsCode('','') == true  ? 1 : 0];
+    }
+
+
+
     public function actionCaptchaCode($mobile)
     {
         \yii::$app->response->format = Response::FORMAT_JSON;
+
+
+
         if (Mobile::check($mobile)) {
+
             $model = new ApiUser;
             $model = $model->find()->where([
                 'Phone'=>$mobile
@@ -41,6 +83,7 @@ class SignController extends Controller
     {
         $this->layout = '@app/views/sign/layouts/sign';
         $model = new ApiUser();
+
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             SmsHelper::sendRegSuccess($model->Phone,$model->UserName,$model->Password);
             return $this->render('in', [
